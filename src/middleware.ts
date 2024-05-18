d2d00409dcc08fcc23dde7a6e7bb6af150f5356e
Matheus Henrique de Oliveira <matheushro.dev@gmail.com>
2024-05-18 16:20:37 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,14 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getUrl } from '@/lib/get-url'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('authjs.session-token')
+  const token = request.cookies.get('__Secure-authjs.session-token')
   const pathname = request.nextUrl.pathname
 
   if (pathname === '/' && token) {
-    console.log({ login:  new URL(getUrl('/app'))})
     return NextResponse.redirect(new URL(getUrl('/app')))
   }
 
   if (pathname.includes('/app') && !token) {
-    console.log({ logout:  new URL(getUrl('/app'))})
     return NextResponse.redirect(new URL(getUrl('/')))
   }
 }
